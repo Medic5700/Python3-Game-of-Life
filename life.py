af316377c72181c76ba975dfc25811b69d01082f
Medic5700 <super_alias_the_first@hotmail.com>
2017-11-22 02:26:13 -0500

    adjustable map dimensions

--- life.py
+++ life.py
@@ -1,20 +1,25 @@
 import random
 import math
 import time
-gameMap = [[(False) for i in range(32)] for i in range(32)]
-delta = [[(False) for i in range(32)] for i in range(32)]
 
+#settings
+mapX = 64
+mapY = 32
+timeout = 512 # -1 for infinit
 
-for i in range(32):
-    for j in range(32):
+#map generation
+gameMap = [[(False) for x in range(mapX)] for y in range(mapY)]
+delta = [[(False) for x in range(mapX)] for y in range(mapY)]
+for y in range(mapY):
+    for x in range(mapX):
         if random.randint(0,7) <= 3:
-            gameMap[i][j] = True
+            gameMap[y][x] = True
             
 def printScreen(gameMap):
     temp = ""
-    for i in range(32):
-        for j in range(32):
-            if gameMap[i][j] == False:
+    for y in range(mapY):
+        for x in range(mapX):
+            if gameMap[y][x] == False:
                 temp += "0"
             else:
                 temp += "1"
@@ -22,55 +27,57 @@ def printScreen(gameMap):
     print(temp)
 
 def rules(gameMap):
-    t1 = [[(0) for i in range(32)] for i in range(32)]
+    t1 = [[(0) for x in range(mapX)] for y in range(mapY)]
     
-    for i in range(32):
-        for j in range(32):
+    for y in range(mapY):
+        for x in range(mapX):
             temp = 0
-            if gameMap[(i - 1) % 32][(j - 1) % 32] == True:
+            if gameMap[(y - 1) % mapY][(x - 1) % mapX] == True:
                 temp += 1
-            if gameMap[(i - 0) % 32][(j - 1) % 32] == True:
+            if gameMap[(y - 0) % mapY][(x - 1) % mapX] == True:
                 temp += 1
-            if gameMap[(i + 1) % 32][(j - 1) % 32] == True:
+            if gameMap[(y + 1) % mapY][(x - 1) % mapX] == True:
                 temp += 1
-            if gameMap[(i - 1) % 32][(j - 0) % 32] == True:
+            if gameMap[(y - 1) % mapY][(x - 0) % mapX] == True:
                 temp += 1
-            if gameMap[(i + 1) % 32][(j - 0) % 32] == True:
+            if gameMap[(y + 1) % mapY][(x - 0) % mapX] == True:
                 temp += 1
-            if gameMap[(i - 1) % 32][(j + 1) % 32] == True:
+            if gameMap[(y - 1) % mapY][(x + 1) % mapX] == True:
                 temp += 1
-            if gameMap[(i - 0) % 32][(j + 1) % 32] == True:
+            if gameMap[(y - 0) % mapY][(x + 1) % mapX] == True:
                 temp += 1
-            if gameMap[(i + 1) % 32][(j + 1) % 32] == True:
+            if gameMap[(y + 1) % mapY][(x + 1) % mapX] == True:
                 temp += 1
                 
-            t1[i][j] = temp
+            t1[y][x] = temp
     
-    for i in range(32):
-        for j in range(32):
-            if gameMap[i][j] == False:
-                if t1[i][j] == 3:
-                    delta[i][j] = True
+    for y in range(mapY):
+        for x in range(mapX):
+            if gameMap[y][x] == False:
+                if t1[y][x] == 3:
+                    delta[y][x] = True
                 else:
-                    delta[i][j] = False
-            if gameMap[i][j] == True:
-                if t1[i][j] < 2 or t1[i][j] > 3:
-                    delta[i][j] = False
+                    delta[y][x] = False
+            if gameMap[y][x] == True:
+                if t1[y][x] < 2 or t1[y][x] > 3:
+                    delta[y][x] = False
                 else:
-                    delta[i][j] = True
+                    delta[y][x] = True
     
     return delta
                 
 def toLife(gameMap):
-    for i in range(32):
-        for j in range(32):
-            if gameMap[i][j] == False and random.randint(0,63) <= 0:
-                gameMap[i][j] = True
+    for y in range(mapY):
+        for x in range(mapX):
+            if gameMap[y][x] == False and random.randint(0,63) <= 0:
+                gameMap[y][x] = True
     return gameMap
 
-while(True):
+t = 0
+while(t == timeout):
     printScreen(gameMap)
     gameMap = rules(gameMap)
     #gameMap = toLife(gameMap)
     time.sleep(0.1)
+    t += 1
     
