308c8eb9e89c40ac851d662900f1a151494bb6a4
Medic5700 <super_alias_the_first@hotmail.com>
2017-11-22 03:47:54 -0500

    Streamlined iteration function
    Streamlined the function that applies the game rules to the game board.

--- life.py
+++ life.py
@@ -28,44 +28,25 @@ def strScreen(gameMap):
         temp += "\n"
     return temp
 
-def rules(gameMap):
+def iterate(gameMap):
     """Takes the gameMap, returns gameMap iterated one step"""
-    t1 = [[(0) for x in range(mapX)] for y in range(mapY)]
+    neighbors = [[(0) for x in range(mapX)] for y in range(mapY)]
     
     for y in range(mapY):
         for x in range(mapX):
             temp = 0
-            if gameMap[(y - 1) % mapY][(x - 1) % mapX] == True:
-                temp += 1
-            if gameMap[(y - 0) % mapY][(x - 1) % mapX] == True:
-                temp += 1
-            if gameMap[(y + 1) % mapY][(x - 1) % mapX] == True:
-                temp += 1
-            if gameMap[(y - 1) % mapY][(x - 0) % mapX] == True:
-                temp += 1
-            if gameMap[(y + 1) % mapY][(x - 0) % mapX] == True:
-                temp += 1
-            if gameMap[(y - 1) % mapY][(x + 1) % mapX] == True:
-                temp += 1
-            if gameMap[(y - 0) % mapY][(x + 1) % mapX] == True:
-                temp += 1
-            if gameMap[(y + 1) % mapY][(x + 1) % mapX] == True:
-                temp += 1
-                
-            t1[y][x] = temp
+            t = [0, 1, 1, 1, 0, -1, -1, -1]
+            for i in range(8):
+                if gameMap[(t[(i+2)%8]+y) % mapY][(t[(i+0)%8]+x) % mapX] == True:
+                    temp += 1
+            neighbors[y][x] = temp
     
     for y in range(mapY):
         for x in range(mapX):
-            if gameMap[y][x] == False:
-                if t1[y][x] == 3:
-                    gameMap[y][x] = True
-                else:
-                    gameMap[y][x] = False
-            if gameMap[y][x] == True:
-                if t1[y][x] < 2 or t1[y][x] > 3:
-                    gameMap[y][x] = False
-                else:
-                    gameMap[y][x] = True
+            if (gameMap[y][x] == False) and (neighbors[y][x] == 3):
+                gameMap[y][x] = True
+            if (gameMap[y][x] == True) and (neighbors[y][x] < 2 or neighbors[y][x] > 3):
+                gameMap[y][x] = False
     return gameMap
 
 if __name__ == "__main__":
@@ -73,6 +54,6 @@ if __name__ == "__main__":
     step = 0
     while(step != timeout):
         print(strScreen(gameMap))
-        gameMap = rules(gameMap)
+        gameMap = iterate(gameMap)
         time.sleep(0.1)
         step += 1
