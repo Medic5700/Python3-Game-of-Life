44829e50b0864bf50faab02b69643e3c5e318198
Medic5700 <super_alias_the_first@hotmail.com>
2017-11-26 22:49:54 -0500

    Global variables cleanup
    functions no longer use global variables maxX, maxY outside of their
    scope

--- life.py
+++ life.py
@@ -12,11 +12,11 @@ mapX = 80 #map width
 mapY = 20 #map height
 timeout = 1024 #max run time in steps, -1 for infinite runtime
 
-def generateMap():
-    """Generates random map, returns a two dimensional array of True or False"""
-    gameMap = [[(False) for x in range(mapX)] for y in range(mapY)]
-    for y in range(mapY):
-        for x in range(mapX): #nested array is always the x (horizontal) line
+def generateMap(width, height):
+    """takes dimensions, Generates random map, returns a two dimensional array of True or False"""
+    gameMap = [[(False) for x in range(width)] for y in range(height)]
+    for y in range(height):
+        for x in range(width): #nested array is always the x (horizontal) line
             if random.randint(0,7) <= 3:
                 gameMap[y][x] = True
     return gameMap
@@ -24,25 +24,25 @@ def generateMap():
 def strScreen(gameMap):
     """Takes the gameMap, returns string representing gameMap for print()"""
     result = ""
-    for y in range(mapY):
+    for y in range(len(gameMap)):
         result += "\n" #newline here because it meshes with previous frame better
-        for x in range(mapX):
+        for x in range(len(gameMap[0])):
             result += ('-' if (gameMap[y][x] == False) else '0')
     return result
 
 def iterate(gameMap):
     """Takes the gameMap, returns gameMap iterated one step"""
-    neighbors = [[(0) for x in range(mapX)] for y in range(mapY)]
+    neighbors = [[(0) for x in range(len(gameMap[0]))] for y in range(len(gameMap))]
     
-    for y in range(mapY):
-        for x in range(mapX):
+    for y in range(len(gameMap)):
+        for x in range(len(gameMap[0])):
             t = [0, 1, 1, 1, 0, -1, -1, -1, 0, 1] #repeating pattern of period 8
             for i in range(8): #checks 8 adjacent cells
-                if gameMap[(t[i+2] + y) % mapY][(t[i] + x) % mapX] == True:
+                if gameMap[(t[i+2] + y) % len(gameMap)][(t[i] + x) % len(gameMap[0])] == True:
                     neighbors[y][x] += 1
     
-    for y in range(mapY):
-        for x in range(mapX): #applies game rules to gameMap for each cell
+    for y in range(len(gameMap)):
+        for x in range(len(gameMap[0])): #applies game rules to gameMap for each cell
             if (gameMap[y][x] == False) and (neighbors[y][x] == 3):
                 gameMap[y][x] = True
             elif (gameMap[y][x] == True) and (neighbors[y][x] < 2 or neighbors[y][x] > 3):
@@ -51,7 +51,7 @@ def iterate(gameMap):
     return gameMap
 
 if __name__ == "__main__":
-    gameMap = generateMap()
+    gameMap = generateMap(mapX, mapY)
     while(timeout != 0):
         print(strScreen(gameMap) + "\n" + str(timeout), end='')
         gameMap = iterate(gameMap)
